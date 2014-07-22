8a6c7e45263e0c48575d1bf558c64451c2ac51cd
Alexandre Fidélis Vieira Bitencourt <alexandrefvb@gmail.com>
2014-07-22 13:28:22 -0300

    Ajuste para acessar os serviços a partir de /api e não /

--- source/enquete-spring-data/src/main/java/br/com/tqi/enquete/resource/RootResource.java
+++ source/enquete-spring-data/src/main/java/br/com/tqi/enquete/resource/RootResource.java
@@ -13,13 +13,13 @@ public class RootResource extends Resource {
 		LinkBuilder linkBuilder = new LinkBuilder();
 		add(linkBuilder.buildSelfFromCurrentRequest());
 		add(linkBuilder.withRel("enquetes")
-				.withPathSegments(EnqueteResource.URI).build());
+				.addPathSegments(EnqueteResource.URI).build());
 		add(linkBuilder.withRel("nova-enquete").withMethod(Method.POST).build());
-		add(linkBuilder.withRel("enquetes-ativas").withMethod(Method.GET)
-				.withPathSegments(EnqueteResource.URI, "ativas").build());
-		add(linkBuilder.withRel("enquetes-finalizadas")
-				.withPathSegments(EnqueteResource.URI, "finalizadas").build());
-		add(linkBuilder.withRel("enquetes-nao-iniciadas")
-				.withPathSegments(EnqueteResource.URI, "nao-iniciadas").build());
+		add(new LinkBuilder().withRel("enquetes-ativas").withMethod(Method.GET)
+				.addPathSegments(EnqueteResource.URI, "ativas").build());
+		add(new LinkBuilder().withRel("enquetes-finalizadas")
+				.addPathSegments(EnqueteResource.URI, "finalizadas").build());
+		add(new LinkBuilder().withRel("enquetes-nao-iniciadas")
+				.addPathSegments(EnqueteResource.URI, "nao-iniciadas").build());
 	}
 }
